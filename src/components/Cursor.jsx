652fce57d9b4d4d03b55d93c7cf3b3aa65130e53
Muganza089 <israelmuganza089@gmail.com>
2026-02-09 15:34:18 +0600

    add custom cursor

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -3,10 +3,34 @@ import { useEffect } from "react";
 export default function Cursor() {
   useEffect(() => {
     const cursor = document.getElementById("cursor");
+    const pupil = cursor.querySelector(".pupil");
+
+    const maxOffset = 6; // distance max vers le bord de l'œil
 
     const move = (e) => {
+      // Position du curseur custom
       cursor.style.left = `${e.clientX}px`;
       cursor.style.top = `${e.clientY}px`;
+
+      // Centre de l'œil
+      const rect = cursor.getBoundingClientRect();
+      const centerX = rect.left + rect.width / 2;
+      const centerY = rect.top + rect.height / 2;
+
+      // Direction regard
+      const dx = e.clientX - centerX;
+      const dy = e.clientY - centerY;
+
+      const distance = Math.sqrt(dx * dx + dy * dy);
+      const ratio = Math.min(distance / 40, 1);
+
+      const offsetX = (dx / distance) * maxOffset * ratio || 0;
+      const offsetY = (dy / distance) * maxOffset * ratio || 0;
+
+      pupil.style.transform = `
+        translate(-50%, -50%)
+        translate(${offsetX}px, ${offsetY}px)
+      `;
     };
 
     window.addEventListener("mousemove", move);
@@ -14,6 +38,7 @@ export default function Cursor() {
     window.addEventListener("mousedown", () =>
       cursor.classList.add("boost")
     );
+
     window.addEventListener("mouseup", () =>
       cursor.classList.remove("boost")
     );
